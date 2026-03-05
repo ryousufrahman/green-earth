da2be1fb290aeb0b8f733474e9efb29d1f515fef
yousufrahman <ryousuf766@gmail.com>
2026-03-05 23:15:55 +0600

    card details is dynamic

--- scripts/index.js
+++ scripts/index.js
@@ -26,22 +26,22 @@ async function loadTrees(){
 function displayTress(trees) {
     trees.forEach((tree)=>{
         const card =document.createElement('div');
-        card.className ="card bg-base-100 shadow-sm"
+        card.className ="card bg-base-100 shadow-sm h-[300px]"
         card.innerHTML =`
         
           
                   <figure>
                      <img
-                     src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                      alt="Shoes" />
+                     class ="h-48 w-full object-cover"
+                     src="${tree.image}"/>
                     </figure>
                     <div class="card-body">
-                      <h2 class="card-title">Card Title</h2>
-                       <p class="line-clamp-1">A card component has a figure, a body part, and inside body there are title and actions parts</p>
-                       <div class="badge badge-accent">Accent</div>
+                      <h2 class="card-title">${tree.name}</h2>
+                       <p class="line-clamp-1">${tree.description}</p>
+                       <div class="badge badge-accent">${tree.category}</div>
                        <div class="flex  justify-between items-center gap-1">
-                        <h2 class="font-bold text-xl text-[#15803D]">50000</h2>
-                         <button class="btn btn-primary bg-[#15803D]">Buy Now</button>
+                        <h2 class="font-bold text-xl text-[#15803D]">${tree.price}</h2>
+                         <button class="btn btn-primary bg-[#15803D]">Cart</button>
                        </div>
                   </div>
               
